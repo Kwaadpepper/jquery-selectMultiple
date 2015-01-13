9d3b30e80af91a4baec495334a2cd5237eb8649f
Jérémy Munsch <jeremy.munsch@gmail.com>
2015-01-13 18:15:59 +0100

    multipleSelect to selectMultiple

--- jquery.selectMultiple.js
+++ jquery.selectMultiple.js
@@ -23,14 +23,14 @@
  * SOFTWARE.
  */
 
-$.fn.multipleSelect = function() {
-  this.attr('select-id',$.fn.multipleSelect._setID());
-  this.on('change',$.fn.multipleSelect._multipleSelect);
+$.fn.selectMultiple = function() {
+  this.attr('select-id',$.fn.selectMultiple._setID());
+  this.on('change',$.fn.selectMultiple._multipleSelect);
   return this;
 };
-$.fn.multipleSelect._data = {};
+$.fn.selectMultiple._data = {};
 
-$.fn.multipleSelect._genID = function() {
+$.fn.selectMultiple._genID = function() {
   var id = '';
   for(var i=0; i<32; i++) {
     id += Math.floor(Math.random()*16).toString(16).toUpperCase();
@@ -38,33 +38,33 @@ $.fn.multipleSelect._genID = function() {
   return id;
 }
 
-$.fn.multipleSelect._setID = function(id) {
+$.fn.selectMultiple._setID = function(id) {
   if(typeof id === "undefined") id = {};
-  id.id = $.fn.multipleSelect._genID();
-  $.fn.multipleSelect._data[id.id] = {array:[],object:{}};
+  id.id = $.fn.selectMultiple._genID();
+  $.fn.selectMultiple._data[id.id] = {array:[],object:{}};
   return id.id;
 }
 
 $.multipleSelectForAll = function() {
   $('select[multiple="true"]').each(function() {
-    $(this).attr('select-id',$.fn.multipleSelect._setID());
+    $(this).attr('select-id',$.fn.selectMultiple._setID());
   });
-  $('select[multiple="true"]').on('change',$.fn.multipleSelect._multipleSelect);
+  $('select[multiple="true"]').on('change',$.fn.selectMultiple._multipleSelect);
 }
 
-$.fn.multipleSelect._multipleSelect = function() {
+$.fn.selectMultiple._multipleSelect = function() {
   var id = {id:$(this).attr('select-id')};
   if(!id) $(this).attr('select-id',setID(id));
   $.each($(this).val(),function(v,k) {
-    if(typeof $.fn.multipleSelect._data[id.id].object[k] !== "undefined") {
-      delete $.fn.multipleSelect._data[id.id].object[k];
+    if(typeof $.fn.selectMultiple._data[id.id].object[k] !== "undefined") {
+      delete $.fn.selectMultiple._data[id.id].object[k];
     } else {
-     $.fn.multipleSelect._data[id.id].object[k] = k;
+     $.fn.selectMultiple._data[id.id].object[k] = k;
     }  
   });
-  $.fn.multipleSelect._data[id.id].array = [];
-  $.each($.fn.multipleSelect._data[id.id].object,function(v2,k2) {
-   $.fn.multipleSelect._data[id.id].array.push(v2);
+  $.fn.selectMultiple._data[id.id].array = [];
+  $.each($.fn.selectMultiple._data[id.id].object,function(v2,k2) {
+   $.fn.selectMultiple._data[id.id].array.push(v2);
   });
-  $(this).val($.fn.multipleSelect._data[id.id].array);
+  $(this).val($.fn.selectMultiple._data[id.id].array);
 };
